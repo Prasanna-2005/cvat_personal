5572d8f7ba701678d9c7d11f962ad14d2f74680e
prasanna.kumar <prasanna.kumar@quantrium.ai>
2026-07-17 13:13:31 +0000

    fix : modify the mount directory for nuclio json key

--- serverless/deploy_cpu.sh
+++ serverless/deploy_cpu.sh
@@ -37,7 +37,7 @@ do
         --env MLFLOW_TRACKING_USERNAME="${MLFLOW_TRACKING_USERNAME}" \
         --env MLFLOW_TRACKING_PASSWORD="${MLFLOW_TRACKING_PASSWORD}" \
         --platform-config '{"attributes": {"network": "cvat_cvat"}}' \
-        --volume ./serverless/custom/template_duplicator:/opt/nuclio/creds
+        --volume ./serverless/custom:/opt/nuclio/creds
 done
 
 nuctl get function --platform local
